e4f2ac87806ca70e5a2d8b33add52bfe28d63437
Alexander <mnmllike@gmail.com>
2016-11-10 17:07:05 +0200

    Start develop admin panel

--- wp-content/plugins/wheel_game/edit_wheel.php
+++ wp-content/plugins/wheel_game/edit_wheel.php
@@ -1,2 +1,44 @@
 <?php
-echo 'here is admin panel';
+global $wpdb;
+//Active Game
+if (isset($_POST['active'])) {
+    $wpdb->query("INSERT INTO `wp_wheel_game`
+          (`firstname`,`lastname`,`tel`,`email`,`ipaddress`,`reward`) 
+   values ('active', 'active', 0000, 'active', 'active', 'active')");
+}
+
+
+//Check Active
+$checkQuery = "SELECT `firstname` FROM `wp_wheel_game` WHERE `firstname`='active'";
+$checkActive = $wpdb->query($checkQuery);
+
+if ($checkActive == 0){
+    $result = '<div>За да продължите напред, трябва да активирата играта! <form method="post" id="active-form"><input type="submit" name="active" value="Активирай"/></form></div>';
+}
+else{
+
+
+?>
+    <div id="activate">
+        <?php echo $result ?>
+    </div>
+
+    <div id="on-off">
+
+        <form method="post">
+            <div>
+                <span class="span-wheel">Включване/Изключване на играта:</span>
+                    <label class="switch">
+                                <input class="checkbox-slider" type="checkbox" name="onoff">
+                          <div class="slider round"></div>
+                    </label>
+            </div>
+            <div>
+                <span class="span-wheel">Период на промоцията:</span>
+                <span>От:</span><input type="datetime-local" name="ondate">
+            </div>
+        </form>
+    </div>
+<?php
+}
+?>
--- wp-content/plugins/wheel_game/index.php
+++ wp-content/plugins/wheel_game/index.php
@@ -11,6 +11,31 @@ Text Domain:
 */
 
 
+
+function wheel_game_activation() {
+    require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+    global $wpdb;
+    $db_table_name = $wpdb->prefix . 'wheel_game';
+    if( $wpdb->get_var( "SHOW TABLES LIKE '$db_table_name'" ) != $db_table_name ) {
+        if ( ! empty( $wpdb->charset ) )
+            $charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+
+		$sql = "CREATE TABLE " . $db_table_name . " (
+			`id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+			`firstname` varchar(30) NOT NULL,
+			`lastname` varchar(30) NOT NULL,
+			`tel` bigint(20) NOT NULL,
+			`email` varchar(50) NOT NULL,
+			`ipaddress` varchar(20) NOT NULL,
+			`reward` varchar(50) NOT NULL,
+			PRIMARY KEY (`id`)
+		) $charset_collate;";
+		dbDelta( $sql );
+	}
+}
+register_activation_hook(__FILE__, 'wheel_game_activation');
+
+
 function addMenu()
 {
     add_menu_page("Edit Wheel", "Edit Wheel", 4, "edit-wheel", "editWheel");
@@ -19,6 +44,7 @@ function addMenu()
 
 function editWheel()
 {
+    echo '<link rel="stylesheet" type="text/css" href="'.site_url().'/wp-content/plugins/wheel_game/css/style.css"/>';
     include "edit_wheel.php";
 }
 
